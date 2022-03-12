f22965099706fe4bf32e12a23576ceedad9f5eb3
Chih-Yu Chen <chihyu1206@gmail.com>
2022-03-12 10:14:12 +0800

    Add new files

---
+++ Debug/FreeRTOS/list.d
@@ -0,0 +1,17 @@
+FreeRTOS/list.o: ../FreeRTOS/list.c \
+ /Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/FreeRTOS.h \
+ /Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/FreeRTOSConfig.h \
+ /Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/projdefs.h \
+ /Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/portable.h \
+ /Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/deprecated_definitions.h \
+ /Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/ARM_CM4F/portmacro.h \
+ /Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/mpu_wrappers.h \
+ /Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/list.h
+/Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/FreeRTOS.h:
+/Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/FreeRTOSConfig.h:
+/Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/projdefs.h:
+/Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/portable.h:
+/Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/deprecated_definitions.h:
+/Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/ARM_CM4F/portmacro.h:
+/Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/mpu_wrappers.h:
+/Users/chihyu/STM32CubeIDE/workspace_1.9.0/Embedded_OS_Lab3_P76091713/FreeRTOS/include/list.h:
